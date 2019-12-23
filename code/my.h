1ca7b4d277870463b861bd9de77f79abb72b3fde
zjt <3066974817@qq.com>
2019-12-23 14:28:29 +0800

    第十五周实验

---
+++ code/my.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#define LOOP 1000000
+#define NUM 4
+
